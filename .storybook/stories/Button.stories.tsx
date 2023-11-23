e2ba972becab30bcdd2a2e98c797f7b25e76169c
IvanS <i.surovtsev22@gmail.com>
2023-11-23 16:53:08 +0500

    Create button component and storybook button

---
+++ .storybook/stories/Button.stories.tsx
@@ -0,0 +1,29 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "ui-kit"
+
+const meta = {
+  title: "Button",
+  component: Button,
+  tags: ["autodocs"],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+/* Base button */
+export const Base: Story = {
+  args: {
+    isDisabled: false,
+    children: "Button",
+  }
+}
+
+/* Disabled button */
+export const Disabled: Story = {
+  args: {
+    isDisabled: true,
+    children: "Button",
+  }
+}
